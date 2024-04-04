2ad672bcfaf7eca3df231822fea8dfd88bf62ed3
Daniel Wasiluk <118384587+daniel-wasiluk@users.noreply.github.com>
2024-04-04 13:52:42 -0400

    Create 1.2 Mathematics.js

---
+++ Unit01/1.2 Mathematics.js
@@ -0,0 +1,35 @@
+// Part 1
+
+//Declare initial variables
+let celsius = 100;
+let fahrenheit = 0;
+
+// Use celcius to fahrenheit formula
+fahrenheit = (celsius * 9/5) + 32;
+
+//print it
+console.log(fahrenheit)
+
+//Part 2
+//change fahrenehit to 77
+fahrenheit = 77;
+
+//use celcius to fahrenheit
+celsius = (fahrenheit - 32) * 5/9;
+
+//print result
+console.log(celsius);
+
+//Part 3
+console.log("77 degrees Fahrenheit is",celsius,"degrees celsius.");
+
+//Last Part
+
+x1 = -4;
+y1 = 5;
+x2 = 6;
+y2 = 0;
+
+m = (y2 - y1)/(x2 - x1);
+
+console.log("The slope is equal to",m);
