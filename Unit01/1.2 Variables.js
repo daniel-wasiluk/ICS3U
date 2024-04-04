d97b90a9407b0a7890db15997c0b8943e5f61fcd
Daniel Wasiluk <118384587+daniel-wasiluk@users.noreply.github.com>
2024-04-04 13:37:47 -0400

    Create 1.2 Variables.js

---
+++ Unit01/1.2 Variables.js
@@ -0,0 +1,21 @@
+//PART 1
+console.log("This is Part 1: \n")
+
+// Setting the length and width, as specified in the markdown
+let length = 10;
+let width = 5;
+
+// area is equal to length times width
+let area =  length * width;
+
+//printed using the two ways specified, spaced by a line break
+console.log(area,"\n");
+console.log("Area:",area,"\n\n");
+
+//PART 2
+console.log("This is Part 2!!!\n")
+let first_name = "Santa";
+var last_name = "Claws";
+var last_name = "Claus";
+
+console.log("I still believe in",first_name,last_name+".")
