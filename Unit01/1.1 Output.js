2828c295cd2db5c40278da274421390ead030aef
Daniel Wasiluk <118384587+daniel-wasiluk@users.noreply.github.com>
2024-04-04 13:38:39 -0400

    Create 1.1 Output.js

---
+++ Unit01/1.1 Output.js
@@ -0,0 +1,18 @@
+//Task 1
+console.log("Apple Inc.");
+console.log("One Apple Park Way");
+console.log("Cupertino, CA");
+console.log("95014");
+console.log("+1 408-996-1010");
+
+//Spacing for next
+console.log("\n\n");
+
+//Task 2
+console.log("Microsoft Corporation\nOne Microsoft Way\nRedmond, WA\n98052\n+1 425-882-8080");
+
+// Spacing for next one
+console.log("\n\n");
+
+//Task 3
+console.log("x\t o\t x\no\t x\t o\nx\t o\t x");
