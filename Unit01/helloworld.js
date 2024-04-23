99e842e8a377154f235b40e7fa6fdf424bc7eb33
Daniel Wasiluk <118384587+daniel-wasiluk@users.noreply.github.com>
2024-04-23 12:39:21 -0400

    Update helloworld.js

--- Unit01/helloworld.js
+++ Unit01/helloworld.js
@@ -1,2 +1,2 @@
-// this uses the console.log built-in operator to output a sentence to the terminal
+// this uses the console.log() built-in javascript function to output a sentence to the terminal
 console.log("Hello Mr. J's World");
